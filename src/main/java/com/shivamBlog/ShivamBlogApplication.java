62650948fb589ffe045ca2adb7c24bb9cc5f634c
shivam <ershivampandey653@gmail.com>
2024-06-14 13:01:27 +0530

    127:create save post feature

---
+++ src/main/java/com/shivamBlog/ShivamBlogApplication.java
@@ -0,0 +1,13 @@
+package com.shivamBlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShivamBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShivamBlogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shivamBlog/ShivamBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shivamBlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShivamBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
